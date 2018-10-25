64fcacbbffc0ee86af145d544e447bd368edc459
Katie French <katiefrench@Katies-MacBook-Pro.local>
2018-10-26 12:59:37 +1300

    Setting up project and grunt files

---
+++ gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+      cssmin: {
+        target: {
+          files: [{
+            expand: true,
+            cwd: 'css/',
+            src: ['*.css', '!*.min.css'],
+            dest: 'css/',
+            ext: '.min.css'
+          }]
+        }
+    },
+    jshint: {
+        files: ["*.js", "js/script.js"],
+        options: {
+            globals:{
+                jQuery: true
+            }
+        }
+    },
+    csslint: {
+        strict: {
+          options: {
+            import: 2
+          },
+          src: ["css/style.css"]
+        }
+    },
+    uglify:{
+        my_target:{
+            files: {
+                "js/script.min.js":["js/script.js"]
+            }
+        }
+    },
+    sass:{
+        dist:{
+            files:{
+                "css/style.css":"sass/style.scss"
+            }
+        }
+    },
+    watch: {
+        css:{
+            files:["css/style.css", "css/sass.css"],
+            tasks:["csslint", "cssmin"]
+        },
+        js:{
+            files:["js/script.js"],
+            tasks:["jshint", "uglify"]
+        },
+        sass:{
+            files:["sass/style.scss"],
+            tasks:["sass"]
+        }
+    }
+  });
+
+  // grunt.loadNpmTasks();
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // grunt.registerTask();
+  grunt.registerTask('default', ['cssmin']);
+  grunt.registerTask('compile', ['sass']);
+  grunt.registerTask('debug', ['jshint', 'csslint']);
+  grunt.registerTask('min', ['csslint', 'cssmin','jshint', 'uglify']);
+  grunt.registerTask('w', ['watch']);
+
+
+
+
+
+
+
+};
